311ecba65735c342520f19cfaef5ddb1f92fb9ef
Aryalekshmi <aryaglekshmi@gmail.com>
2023-06-25 18:35:49 +0400

    api key removed

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
-export const API_KEY = 'PNbbGvvtHdSn9EAMQgGOLNOnPgOJ6UPu';
+export const API_KEY = 'YOUR_API_KEY';
 export const SERVER_URL = 'http://localhost:8000/auth';
 export const NEWS_API = '/api/svc/topstories/v2/';
 export const SEARCH_API = '/api/svc/search/v2/articlesearch.json?q=';
